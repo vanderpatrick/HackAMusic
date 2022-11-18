2a1730448115db2c612ffacdc661d50982acd1db
Massimo Ranalli <ranallimax@gmail.com>
2022-11-18 23:15:37 +0000

    Add function to create random sound sequences

--- assets/js/testing-max-ran.js
+++ assets/js/testing-max-ran.js
@@ -1,14 +1,20 @@
-// function to compare two arrays and return the number of right values
+// the numBeats variable will be function of the game level
+let numBeats = 5;
 
-const randomSong = [1, 2, 3, 1, 4];
+// function to generate randomSong using 6 different sounds, length equal to the numBeats
+function createSong(numBeats) {
+  randomSong = Array.from({length: numBeats}, () => Math.floor((Math.random() * 6) + 1));
+}
+
+// sequence inserted by the user
 const userInput = [1, 4, 2, 1, 3];
 
-let rightAnswers = 0
-
-for (let i = 0; i < randomSong.length; i++) {
-    if (randomSong[i] == userInput[i]) {
-        rightAnswers = rightAnswers + 1;
-      }
-  }
-
-console.log(rightAnswers)
+// function to compare two arrays and return the number of right values
+function checkAnswer(randomSong, userInput) {
+  let rightAnswers = 0;
+  for (let i = 0; i < randomSong.length; i++) {
+      if (randomSong[i] == userInput[i]) {
+          rightAnswers = rightAnswers + 1;
+        }
+    }
+}
