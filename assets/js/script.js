1690eec900e7fb913f14da32d148495507e8f5e4
vanderpatrick <patrick123321123@icloud.com>
2022-11-18 00:26:52 +0000

    Add Audio and transition functionlaity

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
 // Global variables
+let keys = document.querySelectorAll('.event');
 let question_count = 0;
 let next_btn = document.querySelector("#next_btn");
-// Questions array
 
+// Questions array
 let questions = [
   {
     options: ["a", "b", "c", "j", "a"],
@@ -21,16 +22,48 @@ function NextQuestion() {
   }
 }
 
+function PlaySound(event) {
+    const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+  const key = document.querySelector(`button[data-key="${event.keyCode}"]`);
+  if (!audio) return;
+  
+  
+  key.classList.add("playing")
+  audio.currentTime = 0
+  audio.play()
+}
+
+function RemoveTransition(event) {
+    if(event.propertyName == 'transform') return;
+    event.target.classList.remove('playing')
+}
+
+/* Function to set inner html with the content from the questions this 
+function is just the base logic to undertand what the game should do, it will be changed
+*/
 function CallGame(index) {
   let option_text = document.querySelector("#test");
   let option_content =
-    '<div class="btn btn-dark round">' + questions[index].options[0] + "</div>"
-    +'<div class="btn btn-dark round">' + questions[index].options[1] + "</div>"
-    +'<div class="btn btn-dark round">' + questions[index].options[2] + "</div>"
-    +'<div class="btn btn-dark round">' + questions[index].options[3] + "</div>"
-    +'<div class="btn btn-dark round">' + questions[index].options[4] + "</div>"
+    '<div class="btn btn-dark round">' +
+    questions[index].options[0] +
+    "</div>" +
+    '<div class="btn btn-dark round">' +
+    questions[index].options[1] +
+    "</div>" +
+    '<div class="btn btn-dark round">' +
+    questions[index].options[2] +
+    "</div>" +
+    '<div class="btn btn-dark round">' +
+    questions[index].options[3] +
+    "</div>" +
+    '<div class="btn btn-dark round">' +
+    questions[index].options[4] +
+    "</div>";
 
   option_text.innerHTML = option_content;
 }
 
 CallGame(0);
+console.log(keys.values)
+keys.forEach(key => key.addEventListener('transitionend', RemoveTransition));
+window.addEventListener('keydown', PlaySound)
