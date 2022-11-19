89518e23ef677921a396c66259f7ebe9c4c00f18
vanderpatrick <patrick123321123@icloud.com>
2022-11-19 03:48:18 +0000

    Add lv counter and path to game/instruction

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 // Global variables
-let keys = document.querySelectorAll("btn-game")
-let question_count = 0;
-let next_btn = document.querySelector("#next_btn");
+let que_count = 0;
+let current_level = 1;
+let next_btn = document.querySelector(".lv-test");
+
+
 // Questions array
 let questions = [
   {
@@ -14,13 +16,20 @@ let questions = [
     options: ["g", "g", "g", "a", "g"],
   },
 ];
-function NextQuestion() {
-  if (question_count < questions.length - 1) {
-    question_count++;
-    CallGame(question_count);
-  }
+function next_level() {
+  next_btn.onclick = () => {
+    if (que_count < questions.length - 1) {
+      que_count++;
+      current_level++;
+      questionCounter(current_level);
+    } 
+  };
 }
-
+function questionCounter(index) {
+  const question_counter = document.querySelector(".level-current");
+  let question_counter_content =  index 
+  question_counter.innerHTML = question_counter_content;
+}
 /* Function to set inner html with the content from the questions this
 function is just the base logic to undertand what the game should do, it will be changed
 */
@@ -44,21 +53,34 @@ function CallGame(index) {
     "</div>";
 }
 
-function PlaySound(event){
-  let audio = document.querySelector(`audio[data-key="${event.keyCode}"]`)
-  let key = document.querySelector(`div[data-key="${event.keyCode}"]`)
+function PlaySound(event) {
+  let audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+  let key = document.querySelector(`div[data-key="${event.keyCode}"]`);
   if (!audio) return;
-  key.classList.add("active")
-  audio.currentTime = 0
-  audio.play()
+  key.classList.add("active");
+  audio.currentTime = 0;
+  audio.play();
 }
 
-function removeClass (event) {
+function removeClass(event) {
   const key = document.querySelector(`div[data-key="${event.keyCode}"]`);
-  key.classList.remove('active')
+  key.classList.remove("active");
 }
-
+function path_game() {
+  let link = document.querySelector('.link1')
+  link.onclick = () => {
+    window.location.href ='game.html'
+  }
+}
+function path_instruction() {
+  let link = document.querySelector('.link2')
+  link.onclick = () => {
+    window.location.href ='instructions.html'
+  }
+}
+path_game()
+path_instruction()
 CallGame(0);
-console.log(keys.values)
-window.addEventListener('keydown', PlaySound)
-window.addEventListener('keyup', removeClass)
+next_level(0);
+window.addEventListener("keydown", PlaySound);
+window.addEventListener("keyup", removeClass);
